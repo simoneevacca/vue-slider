6f1dfdde92cca91002000e5b2c66a0fc1798bd76
simoneevacca <v.simone.vacca@gmail.com>
2024-03-12 15:05:25 +0100

    add event on click prev and next button

--- js/script.js
+++ js/script.js
@@ -13,19 +13,19 @@ createApp(
                     },
                     {
                         img: "img/02.webp",
-                        title: "Spiderman"
+                        title: "Ratchet & Clank"
                     },
                     {
                         img: "img/03.webp",
-                        title: "Spiderman"
+                        title: "Fortnite"
                     },
                     {
                         img: "img/04.webp",
-                        title: "Spiderman"
+                        title: "Stray"
                     },
                     {
                         img: "img/05.webp",
-                        title: "Spiderman"
+                        title: "Avengers"
                     },
                     
                 ],
@@ -33,11 +33,24 @@ createApp(
             }
         },
 
+        methods:{
+            prevImage() {
+                this.imageCounter--;
+                if (this.imageCounter == 0) {
+                    this.imageCounter === this.images.lenght
+                }
+            },
+
+            nextImage() {
+                this.imageCounter++;
+                if (this.imageCounter == this.images.lenght) {
+                    this.imageCounter = 0
+                }
+            }
+        },
 
         
-        mounted() {
-            console.log(this.images);
-        }
+       
     }
 
 ).mount('#app')
