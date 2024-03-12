1409f71502b16bbe631de677341c2815b1a89272
simoneevacca <v.simone.vacca@gmail.com>
2024-03-12 15:53:34 +0100

    add change image on click preview

--- js/script.js
+++ js/script.js
@@ -1,59 +1,59 @@
 const { createApp } = Vue
 
 
-createApp(
-    {
-        data() {
-            return {
-                imageCounter: 0,
-                images: [
-                    {
-                        img: "img/01.webp",
-                        title: "Spiderman"
-                    },
-                    {
-                        img: "img/02.webp",
-                        title: "Ratchet & Clank"
-                    },
-                    {
-                        img: "img/03.webp",
-                        title: "Fortnite"
-                    },
-                    {
-                        img: "img/04.webp",
-                        title: "Stray"
-                    },
-                    {
-                        img: "img/05.webp",
-                        title: "Avengers"
-                    },
-                    
-                ],
+createApp({
 
+    data() {
+        return {
+            imageCounter: 0,
+            images: [
+                {
+                    img: "img/01.webp",
+                    title: "Spiderman"
+                },
+                {
+                    img: "img/02.webp",
+                    title: "Ratchet & Clank"
+                },
+                {
+                    img: "img/03.webp",
+                    title: "Fortnite"
+                },
+                {
+                    img: "img/04.webp",
+                    title: "Stray"
+                },
+                {
+                    img: "img/05.webp",
+                    title: "Avengers"
+                },
+
+            ],
+
+        }
+    },
+
+    methods: {
+
+
+        prevImage() {
+            if (this.imageCounter == 0) {
+                this.imageCounter = this.images.length
+            }
+            this.imageCounter--;
+        },
+
+        nextImage() {
+            this.imageCounter++;
+            if (this.imageCounter == this.images.length) {
+                this.imageCounter = 0
             }
         },
 
-        methods:{
-          
 
-            prevImage() {
-                if (this.imageCounter == 0) {
-                    this.imageCounter = this.images.length
-                }
-                this.imageCounter--;
-            },
+    },
 
-            nextImage() {
-                this.imageCounter++;
-                if (this.imageCounter == this.images.length) {
-                    this.imageCounter = 0
-                }
-            }
-        },
 
-        
-       
-    }
 
-).mount('#app')
+}).mount('#app')
 
